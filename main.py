aef466005ea9a210d764a83082e125c181429ecb
Justin Nguyen <nguyenjustin581@gmail.com>
2026-04-27 17:05:23 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -27,23 +27,23 @@ tabControl.add(departments_tab, text="Departments")
 tabControl.pack(expand=1, fill="both")
 
 # students tab
-students_table = ttk.Treeview(students_tab, columns=("student_id", "first_name", "last_name", "major"), show = "headings", height=8)
+students_table = ttk.Treeview(students_tab, columns=("student_id", "first_name", "last_name", "major"), show = "headings", height=10)
 
 style = ttk.Style()
 
 students_table.heading("student_id", text="Student ID")
-students_table.column("student_id", width=75)
+students_table.column("student_id", width=172, anchor="center")
 
 students_table.heading("first_name", text="First Name")
-students_table.column("first_name", width=75)
+students_table.column("first_name", width=172, anchor="center")
 
 students_table.heading("last_name", text="Last Name")
-students_table.column("last_name", width=75)
+students_table.column("last_name", width=172, anchor="center")
 
 students_table.heading("major", text="Major")
-students_table.column("major", width=150)
+students_table.column("major", width=172, anchor="center")
 
-students_table.place(x=130, y=150)
+students_table.place(y=150)
 
 student_first_label = ttk.Label(students_tab, text="First Name:")
 student_first_label.pack(padx=10, pady=2)
@@ -94,23 +94,23 @@ update_student_button.pack(padx=10, pady=5)
 update_student_button.place(x=395, y=110)
 
 # courses tab
-courses_table = ttk.Treeview(courses_tab, columns=("course_id", "department_id", "course_name", "credits"), show = "headings", height=8)
+courses_table = ttk.Treeview(courses_tab, columns=("course_id", "department_id", "course_name", "credits"), show = "headings", height=10)
 
 style = ttk.Style()
 
 courses_table.heading("course_id", text="Course ID")
-courses_table.column("course_id", width=75)
+courses_table.column("course_id", width=172, anchor="center")
 
 courses_table.heading("department_id", text="Department ID")
-courses_table.column("department_id", width=75)
+courses_table.column("department_id", width=172, anchor="center")
 
 courses_table.heading("course_name", text="Course Name")
-courses_table.column("course_name", width=175)
+courses_table.column("course_name", width=172, anchor="center")
 
 courses_table.heading("credits", text="Credits")
-courses_table.column("credits", width=50)
+courses_table.column("credits", width=172, anchor="center")
 
-courses_table.place(x=130, y=150)
+courses_table.place(y=150)
 
 course_id_label = ttk.Label(courses_tab, text="Course ID:")
 course_id_label.pack(padx=10, pady=2)
@@ -161,23 +161,23 @@ update_course_button.pack(padx=10, pady=5)
 update_course_button.place(x=395, y=110)
 
 # instructors tab
-instructors_table = ttk.Treeview(instructors_tab, columns=("instructor_id", "department_id", "first_name", "last_name"), show = "headings", height=8)
+instructors_table = ttk.Treeview(instructors_tab, columns=("instructor_id", "department_id", "first_name", "last_name"), show = "headings", height=10)
 
 style = ttk.Style()
 
 instructors_table.heading("instructor_id", text="Instructor ID")
-instructors_table.column("instructor_id", width=100)
+instructors_table.column("instructor_id", width=172)
 
 instructors_table.heading("department_id", text="Department ID")
-instructors_table.column("department_id", width=100)
+instructors_table.column("department_id", width=172)
 
 instructors_table.heading("first_name", text="First Name")
-instructors_table.column("first_name", width=75)
+instructors_table.column("first_name", width=172)
 
 instructors_table.heading("last_name", text="Last Name")
-instructors_table.column("last_name", width=150)
+instructors_table.column("last_name", width=172)
 
-instructors_table.place(x=110, y=150)
+instructors_table.place(y=150)
 
 instructor_id_label = ttk.Label(instructors_tab, text="Instructor ID:")
 instructor_id_label.pack(padx=10, pady=2)
@@ -228,39 +228,39 @@ update_instructor_button.pack(padx=10, pady=5)
 update_instructor_button.place(x=395, y=110)
 
 # departments tab
-departments_table = ttk.Treeview(departments_tab, columns=("department_id", "department_name", "location"), show = "headings", height=8)
+departments_table = ttk.Treeview(departments_tab, columns=("department_id", "department_name", "location"), show = "headings", height=10)
 
 style = ttk.Style()
 
 departments_table.heading("department_id", text="Department ID")
-departments_table.column("department_id", width=100)
+departments_table.column("department_id", width=233)
 
 departments_table.heading("department_name", text="Department Name")
-departments_table.column("department_name", width=100)
+departments_table.column("department_name", width=233)
 
 departments_table.heading("location", text="Location")
-departments_table.column("location", width=100)
+departments_table.column("location", width=233)
 
-departments_table.place(x=130, y=150)
+departments_table.place(y=150)
 
 # enrollments table
-enrollments_table = ttk.Treeview(enrollments_tab, columns=("enrollment_id", "course_id", "student_id", "grade"), show = "headings", height=8)
+enrollments_table = ttk.Treeview(enrollments_tab, columns=("enrollment_id", "course_id", "student_id", "grade"), show = "headings", height=10)
 
 style = ttk.Style()
 
 enrollments_table.heading("enrollment_id", text="Enrollment ID")
-enrollments_table.column("enrollment_id", width=75)
+enrollments_table.column("enrollment_id", width=172)
 
 enrollments_table.heading("course_id", text="Course ID")
-enrollments_table.column("course_id", width=75)
+enrollments_table.column("course_id", width=172)
 
 enrollments_table.heading("student_id", text="Student ID")
-enrollments_table.column("student_id", width=75)
+enrollments_table.column("student_id", width=172)
 
 enrollments_table.heading("grade", text="Grade")
-enrollments_table.column("grade", width=150)
+enrollments_table.column("grade", width=172)
 
-enrollments_table.place(x=130, y=150)
+enrollments_table.place(y=150)
 
 def refresh_students_table():
     for i in students_table.get_children():
